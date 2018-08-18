b7eb934a97ad82c7afccb76dded6d94143e7f411
Lizanoskova <enoskovamipt@gmail.com>
2018-08-18 21:28:22 +0300

    heroku

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
   devServer: {
     disableHostCheck: true,
     host: '0.0.0.0',
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 3030,
   },
 
   watch: NODE_ENV === 'development',
